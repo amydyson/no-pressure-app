422893391500d8629a3eea698abd6333a97548b2
amy <amybethdyson@gmail.com>
2025-08-16 14:10:45 -0700

    beginning of patient form with react hook forma and zod

--- src/components/Patient.tsx
+++ src/components/Patient.tsx
@@ -1,7 +1,64 @@
+import { useForm } from "react-hook-form";
+import { TextField, FormHelperText } from "@mui/material";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  firstName: z
+    .string()
+    .min(2, { message: "First name must be at least 2 characters long" })
+    .max(100, { message: "First name must not be longer than 100 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "Last name must be at least 2 characters long" })
+    .max(100, { message: "Last name must not be longer than 100 characters" }),
+});
+
+type FormData = z.infer<typeof schema>;
 const Patient = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <div>Patient!!!</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="First Name"
+          {...register("firstName")}
+          error={!!errors.firstName}
+          margin="normal"
+          fullWidth
+        />
+        {errors.firstName && (
+          <FormHelperText sx={{ color: "error.main" }}>
+            {errors.firstName.message}
+          </FormHelperText>
+        )}
+        <TextField
+          label="Last Name"
+          {...register("lastName")}
+          error={!!errors.lastName}
+          margin="normal"
+          fullWidth
+        />
+        {errors.lastName && (
+          <FormHelperText sx={{ color: "error.main" }}>
+            {errors.lastName.message}
+          </FormHelperText>
+        )}
+        <button onClick={handleSubmit(onSubmit)} type="submit">
+          Submit
+        </button>
+      </form>
     </>
   );
 };
