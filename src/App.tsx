9ec39fbbd2d7bc1c839ecba5cae7578031b4d70f
amy <amybethdyson@gmail.com>
2025-10-23 20:49:33 -0700

    make upper left go to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
 import Home from "./components/common/Home";
 import SignOut from "./components/common/SignOut";
 import CssBaseline from "@mui/material/CssBaseline";
 import { AppBar, Toolbar, Typography, Box, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import logo from "./assets/images/illustrations/logo-pumpkin.png";
+
+// Navigation Bar component that can use useNavigate
+function NavigationBar() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <AppBar position="sticky" sx={{ 
+      mb: 0,
+      bgcolor: '#2F4F4F',
+      color: '#FFFFFF'
+    }}>
+      <Toolbar sx={{ 
+        flexDirection: { xs: 'column', sm: 'row' }, 
+        gap: { xs: 1, sm: 0 },
+        py: { xs: 1, sm: 0 }
+      }}>
+        <Box sx={{ 
+          flexGrow: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: { xs: 'center', sm: 'flex-start' },
+          gap: 2,
+          cursor: 'pointer',
+          '&:hover': {
+            opacity: 0.8
+          }
+        }}
+        onClick={handleLogoClick}
+        >
+          <img 
+            src={logo} 
+            alt="No Pressure Logo" 
+            style={{ 
+              height: '48px', 
+              width: 'auto'
+            }} 
+          />
+          <Typography variant="h6" component="div">
+            No Pressure
+          </Typography>
+        </Box>
+        <Box>
+          <SignOut />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
 
 function App() {
   return (
@@ -17,43 +69,10 @@ function App() {
           position: 'relative'
         }}>
           <CssBaseline />
-      <AppBar position="sticky" sx={{ 
-        mb: 0,
-        bgcolor: '#2F4F4F',
-        color: '#FFFFFF'
-      }}>
-        <Toolbar sx={{ 
-          flexDirection: { xs: 'column', sm: 'row' }, 
-          gap: { xs: 1, sm: 0 },
-          py: { xs: 1, sm: 0 }
-        }}>
-          <Box sx={{ 
-            flexGrow: 1,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: { xs: 'center', sm: 'flex-start' },
-            gap: 2
-          }}>
-            <img 
-              src={logo} 
-              alt="No Pressure Logo" 
-              style={{ 
-                height: '48px', 
-                width: 'auto'
-              }} 
-            />
-            <Typography variant="h6" component="div">
-              No Pressure
-            </Typography>
-          </Box>
-          <Box>
-            <SignOut />
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Home />
+          <NavigationBar />
+          <Home />
         </Box>
-    </BrowserRouter>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
