0747c5267c6c963fd8de4549a061388b3462c56c
amy <amybethdyson@gmail.com>
2025-11-12 21:14:04 -0800

    remove unused button

--- src/components/patient/HowToGetAReading.tsx
+++ src/components/patient/HowToGetAReading.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import LanguageContext from "../../LanguageContext";
